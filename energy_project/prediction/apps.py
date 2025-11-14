ee4cd2ab946cb449d764456e46c1b219548e4f36
Anugya Verma <119095704+anugyaverma99@users.noreply.github.com>
2025-11-14 20:54:21 +0530

    Update apps.py

--- energy_project/prediction/apps.py
+++ energy_project/prediction/apps.py
@@ -3,5 +3,6 @@ from django.apps import AppConfig
 
 class PredictionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'energy_project.prediction'
+    name = 'prediction'
+
 
